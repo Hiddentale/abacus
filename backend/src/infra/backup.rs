a6b35625d341315f5e479b6628f8348863787051
Hiddentale <55276630+Hiddentale@users.noreply.github.com>
2026-05-21 17:38:05 +0200

    feat: partial work

--- backend/src/infra/backup.rs
+++ backend/src/infra/backup.rs
@@ -1,7 +1,28 @@
 use sqlx::SqlitePool;
+use std::error::Error;
 
-pub async fn backup_database(pool: &SqlitePool, destination: &str) -> Result<(), sqlx::Error> {
+pub struct BackupConfig {
+    pub directory: &str,
+}
+
+async fn backup_database(pool: &SqlitePool, destination: &str) -> Result<(), sqlx::Error> {
     let mut connection = pool.acquire().await?;
     sqlx::query("VACUUM INTO ?").bind(destination).execute(&mut *connection).await?;
+    Ok(())
+}
+
+async fn create_backup_directory() {
+    todo!();
+}
+
+pub async fn store_backup_directory_path() -> Result<(), Box<dyn Error>> {
+    let executable_path = std::env::current_exe()?;
+    let executable_directory = executable_path.parent().ok_or("parent does not exist")?;
+    let backup_directory = executable_directory.join("database_backups");
+    let destination = backup_directory.to_str().ok_or("error formatting string")?;
+}
+
+pub async fn create_local_backup(pool: &SqlitePool) -> Result<(), Box<dyn Error>> {
+    backup_database(pool, backupconfig.directory).await?;
     Ok(())
 }
