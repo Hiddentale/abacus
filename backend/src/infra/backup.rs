c3d7e8c45e3c365361cc90aaceebc4b645781db8
Hiddentale <55276630+Hiddentale@users.noreply.github.com>
2026-06-08 12:19:28 +0200

    feat: oldest backup deletion

--- backend/src/infra/backup.rs
+++ backend/src/infra/backup.rs
@@ -2,7 +2,7 @@ use chrono::Local;
 use sqlx::SqlitePool;
 use std::path::{Path, PathBuf};
 
-const AMOUNT_OF_BACKUP_FILES_ALLOWED: u64 = 5;
+const AMOUNT_OF_BACKUP_FILES_ALLOWED: usize = 5;
 
 #[derive(Clone)]
 pub struct BackupConfig {
@@ -42,6 +42,25 @@ impl From<sqlx::Error> for BackupError {
     }
 }
 
+pub async fn create_local_backup(
+    pool: &SqlitePool,
+    backup_config: &BackupConfig,
+) -> Result<(), BackupError> {
+    create_backup_directory(backup_config)?;
+    let filename = format!("backup_{}.db", Local::now().format("%Y-%m-%d_%H-%M-%S"));
+    let backup_path = &backup_config.directory.join(filename);
+    backup_database(pool, &backup_path).await?;
+    if count_backups(&backup_config)? > AMOUNT_OF_BACKUP_FILES_ALLOWED {
+        remove_oldest_backup(&backup_config)?;
+    }
+    Ok(())
+}
+
+fn create_backup_directory(backup_config: &BackupConfig) -> Result<(), BackupError> {
+    std::fs::create_dir_all(&backup_config.directory)?;
+    Ok(())
+}
+
 async fn backup_database(pool: &SqlitePool, destination: &Path) -> Result<(), BackupError> {
     let destination = destination
         .to_str()
@@ -55,6 +74,12 @@ async fn backup_database(pool: &SqlitePool, destination: &Path) -> Result<(), Ba
     Ok(())
 }
 
+fn count_backups(backup_config: &BackupConfig) -> Result<usize, BackupError> {
+    let directory_entries = std::fs::read_dir(&backup_config.directory)?;
+    let count = directory_entries.filter_map(|f| Some(f.ok()?)).count();
+    Ok(count)
+}
+
 fn remove_oldest_backup(backup_config: &BackupConfig) -> Result<(), BackupError> {
     let directory_entries = std::fs::read_dir(&backup_config.directory)?;
     let oldest_backup = directory_entries.filter_map(|f| Some(f.ok()?.path())).min();
@@ -64,26 +89,11 @@ fn remove_oldest_backup(backup_config: &BackupConfig) -> Result<(), BackupError>
     Ok(())
 }
 
-fn create_backup_directory(backupconfig: &BackupConfig) -> Result<(), BackupError> {
-    std::fs::create_dir_all(&backupconfig.directory)?;
-    Ok(())
-}
-
-pub async fn create_local_backup(
-    pool: &SqlitePool,
-    backupconfig: &BackupConfig,
-) -> Result<(), BackupError> {
-    create_backup_directory(backupconfig)?;
-    let filename = format!("backup_{}.db", Local::now().format("%Y-%m-%d_%H-%M-%S"));
-    let backup_path = backupconfig.directory.join(filename);
-    backup_database(pool, &backup_path).await?;
-    Ok(())
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tokio::sync::Mutex;
+    use tokio::time::Duration;
+    use tokio::{sync::Mutex, time::sleep};
 
     static TEST_LOCK: Mutex<()> = Mutex::const_new(());
 
@@ -129,6 +139,29 @@ mod tests {
         create_backup_directory(&backup_config).unwrap();
         create_local_backup(&pool, &backup_config).await.unwrap();
         assert!(backup_config.directory.read_dir().unwrap().next().is_some());
+        delete_backup_directory(&backup_config);
+    }
+
+    #[sqlx::test]
+    async fn local_backup_count_is_correct(pool: SqlitePool) {
+        let _lock = TEST_LOCK.lock().await;
+        let backup_config = setup_backup_config();
+        create_backup_directory(&backup_config).unwrap();
+        create_local_backup(&pool, &backup_config).await.unwrap();
+        assert_eq!(count_backups(&backup_config).unwrap(), 1);
+        delete_backup_directory(&backup_config);
+    }
+
+    #[sqlx::test]
+    async fn oldest_backup_is_removed_when_limit_is_exceeded(pool: SqlitePool) {
+        let _lock = TEST_LOCK.lock().await;
+        let backup_config = setup_backup_config();
+        create_backup_directory(&backup_config).unwrap();
+        for _ in 0..6 {
+            create_local_backup(&pool, &backup_config).await.unwrap();
+            sleep(Duration::from_secs(1)).await;
+        }
+        assert_eq!(count_backups(&backup_config).unwrap(), 5);
         delete_backup_directory(&backup_config);
     }
 
